4da1d35c93c561821fbd9830e76f4caa7466a4b5
ALI RAZA KARIMI <123482649+razaalikarimi@users.noreply.github.com>
2025-12-02 00:26:20 +0530

    Update getCreatorCourseData.jsx

--- frontend/src/customHooks/getCreatorCourseData.jsx
+++ frontend/src/customHooks/getCreatorCourseData.jsx
@@ -1,33 +1,44 @@
-import React, { useEffect } from 'react'
-import { serverUrl } from '../App'
-import axios from 'axios'
-import { setCreatorCourseData } from '../redux/courseSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import { toast } from 'react-toastify'
 
-const getCreatorCourseData = () => {
-    const dispatch = useDispatch()
-    const {userData} = useSelector(state=>state.user)
-  return (
-    useEffect(()=>{
+
+import { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { setCreatorCourseData } from "../redux/courseSlice";
+import { serverUrl } from "../App";
+
+const useCreatorCourseData = () => {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+
+  useEffect(() => {
+   
+    if (!userData || !userData._id) return;
+
     const getCreatorData = async () => {
       try {
-        const result = await axios.get(serverUrl + "/api/course/getcreatorcourses" , {withCredentials:true})
-        
-         await dispatch(setCreatorCourseData(result.data))
+        const result = await axios.get(
+          serverUrl + "/api/course/getcreatorcourses",
+          { withCredentials: true }
+        );
 
-        
-        console.log(result.data)
-        
+        console.log("creator courses:", result.data);
+        dispatch(setCreatorCourseData(result.data));
       } catch (error) {
-        console.log(error)
-        toast.error(error.response.data.message)
-      }
-      
-    }
-    getCreatorData()
-  },[userData])
-  )
-}
+        console.log("creatorcourses error:", {
+          status: error.response?.status,
+          data: error.response?.data,
+          message: error.message,
+        });
 
-export default getCreatorCourseData
+        toast.error(
+          error?.response?.data?.message || "Failed to load creator courses"
+        );
+      }
+    };
+
+    getCreatorData();
+  }, [dispatch, userData]);
+};
+
+export default useCreatorCourseData;
