70bddc98633515e2f446221826d47e9db0010331
ALI RAZA KARIMI <123482649+razaalikarimi@users.noreply.github.com>
2025-12-02 00:25:09 +0530

    Update getCurrentUser.jsx

--- frontend/src/customHooks/getCurrentUser.jsx
+++ frontend/src/customHooks/getCurrentUser.jsx
@@ -1,24 +1,38 @@
-import { useEffect } from "react"
-import { serverUrl } from "../App"
-import axios from "axios"
-import { useDispatch, useSelector } from "react-redux"
-import { setUserData } from "../redux/userSlice"
-const getCurrentUser = ()=>{
-    let dispatch = useDispatch()
+
+
+import { useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../redux/userSlice";
+import { serverUrl } from "../App";
+
+const useCurrentUser = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const result = await axios.get(
+          serverUrl + "/api/user/currentuser",
+          { withCredentials: true }
+        );
+
+        console.log("currentuser result:", result.data);
+
    
-    useEffect(()=>{
-        const fetchUser = async () => {
-            try {
-                let result = await axios.get(serverUrl + "/api/user/currentuser" , {withCredentials:true})
-                dispatch(setUserData(result.data))
+        dispatch(setUserData(result.data.user || result.data));
+      } catch (error) {
+        console.log("currentuser error:", {
+          status: error.response?.status,
+          data: error.response?.data,
+          message: error.message,
+        });
+        dispatch(setUserData(null));
+      }
+    };
 
-            } catch (error) {
-                console.log(error)
-                dispatch(setUserData(null))
-            }
-        }
-        fetchUser()
-    },[])
-}
+    fetchUser();
+  }, [dispatch]);
+};
 
-export default getCurrentUser
+export default useCurrentUser;
