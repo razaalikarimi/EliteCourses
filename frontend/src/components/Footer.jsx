23423c219ef016f60957669b0caf69492f0ff15d
razaalikarimi <aliraza85211@gmail.com>
2025-12-17 23:00:49 +0530

    update commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,13 +7,17 @@ const Footer = () => {
   return (
     <footer className="bg-black text-gray-300 py-10 px-6">
       <div className="max-w-7xl mx-auto flex lg:items-center items-start justify-center gap-[40px] lg:gap-[150px] flex-col lg:flex-row">
-
         {/* Logo + Description */}
         <div className="lg:w-[40%] md:w-[50%] w-[100%]">
-          <img src={logo} alt="Logo" className="h-10 mb-3 border-1 rounded-[5px]" />
-          <h2 className="text-xl font-bold text-white mb-3">Virtual Courses</h2>
+          <img
+            src={logo}
+            alt="Logo"
+            className="h-10 mb-3 border-1 rounded-[5px]"
+          />
+          <h2 className="text-xl font-bold text-white mb-3">Elite Courses</h2>
           <p className="text-sm">
-            AI-powered learning platform to help you grow smarter. Learn anything, anytime, anywhere.
+            AI-powered learning platform to help you grow smarter. Learn
+            anything, anytime, anywhere.
           </p>
         </div>
 
@@ -21,10 +25,30 @@ const Footer = () => {
         <div className="lg:w-[30%] md:w-[100%]">
           <h3 className="text-white font-semibold mb-2">Quick Links</h3>
           <ul className="space-y-1 text-sm">
-            <li className="hover:text-white cursor-pointer" onClick={() => navigate("/")}>Home</li>
-            <li className="hover:text-white cursor-pointer" onClick={() => navigate("/allcourses")}>Courses</li>
-            <li className="hover:text-white cursor-pointer" onClick={() => navigate("/login")}>Login</li>
-            <li className="hover:text-white cursor-pointer" onClick={() => navigate("/profile")}>My Profile</li>
+            <li
+              className="hover:text-white cursor-pointer"
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className="hover:text-white cursor-pointer"
+              onClick={() => navigate("/allcourses")}
+            >
+              Courses
+            </li>
+            <li
+              className="hover:text-white cursor-pointer"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </li>
+            <li
+              className="hover:text-white cursor-pointer"
+              onClick={() => navigate("/profile")}
+            >
+              My Profile
+            </li>
           </ul>
         </div>
 
@@ -38,7 +62,6 @@ const Footer = () => {
             <li className="hover:text-white">UI/UX Design</li>
           </ul>
         </div>
-
       </div>
 
       {/* Bottom Bar */}
