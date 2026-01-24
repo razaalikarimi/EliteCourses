eaae11d8fd53f1cca8594f53c89f11f0479f6bfa
ALI RAZA KARIMI <123482649+razaalikarimi@users.noreply.github.com>
2026-01-24 23:58:31 +0530

    Update getCurrentUser.jsx

--- frontend/src/customHooks/getCurrentUser.jsx
+++ frontend/src/customHooks/getCurrentUser.jsx
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-// src/hooks/getCurrentUser.jsx  (ya jaha bhi file rakhi hai)
+// src/hooks/useCurrentUser.jsx
 
 import { useEffect } from "react";
 import axios from "axios";
@@ -19,7 +18,7 @@ const useCurrentUser = () => {
 
         console.log("currentuser result:", result.data);
 
-        // Agar backend { user: {...} } bhej raha hai:
+        // backend may send { user: {...} } or direct user object
         dispatch(setUserData(result.data.user || result.data));
       } catch (error) {
         console.log("currentuser error:", {
@@ -27,6 +26,7 @@ const useCurrentUser = () => {
           data: error.response?.data,
           message: error.message,
         });
+
         dispatch(setUserData(null));
       }
     };
@@ -35,43 +35,4 @@ const useCurrentUser = () => {
   }, [dispatch]);
 };
 
-=======
-
-
-import { useEffect } from "react";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { setUserData } from "../redux/userSlice";
-import { serverUrl } from "../App";
-
-const useCurrentUser = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const result = await axios.get(
-          serverUrl + "/api/user/currentuser",
-          { withCredentials: true }
-        );
-
-        console.log("currentuser result:", result.data);
-
-   
-        dispatch(setUserData(result.data.user || result.data));
-      } catch (error) {
-        console.log("currentuser error:", {
-          status: error.response?.status,
-          data: error.response?.data,
-          message: error.message,
-        });
-        dispatch(setUserData(null));
-      }
-    };
-
-    fetchUser();
-  }, [dispatch]);
-};
-
->>>>>>> 6a7a943fc54e9f14262178af4c8079e6c1d01555
 export default useCurrentUser;
