b3293087d2792ad6117c0ef1cdf35d3e92eb5cff
ALI RAZA KARIMI <123482649+razaalikarimi@users.noreply.github.com>
2026-01-24 23:58:10 +0530

    Update getCreatorCourseData.jsx

--- frontend/src/customHooks/getCreatorCourseData.jsx
+++ frontend/src/customHooks/getCreatorCourseData.jsx
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-// src/hooks/getCreatorCourseData.jsx
+// src/hooks/useCreatorCourseData.jsx
 
 import { useEffect } from "react";
 import axios from "axios";
@@ -13,46 +12,26 @@ const useCreatorCourseData = () => {
   const { userData } = useSelector((state) => state.user);
 
   useEffect(() => {
-   
     if (!userData || !userData._id) return;
 
-=======
-
-
-import { useEffect } from "react";
-import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { toast } from "react-toastify";
-import { setCreatorCourseData } from "../redux/courseSlice";
-import { serverUrl } from "../App";
-
-const useCreatorCourseData = () => {
-  const dispatch = useDispatch();
-  const { userData } = useSelector((state) => state.user);
-
-  useEffect(() => {
-   
-    if (!userData || !userData._id) return;
-
->>>>>>> 6a7a943fc54e9f14262178af4c8079e6c1d01555
     const getCreatorData = async () => {
       try {
         const result = await axios.get(
           serverUrl + "/api/course/getcreatorcourses",
-          { withCredentials: true }
+          { withCredentials: true },
         );
 
         console.log("creator courses:", result.data);
         dispatch(setCreatorCourseData(result.data));
       } catch (error) {
-        console.log("creatorcourses error:", {
+        console.log("creator courses error:", {
           status: error.response?.status,
           data: error.response?.data,
           message: error.message,
         });
 
         toast.error(
-          error?.response?.data?.message || "Failed to load creator courses"
+          error?.response?.data?.message || "Failed to load creator courses",
         );
       }
     };
