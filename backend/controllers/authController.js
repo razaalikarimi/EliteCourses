5af6cc268e2921e5accc5c63dd2d4dae7f068026
razaalikarimi <aliraza85211@gmail.com>
2026-04-28 14:03:37 +0530

    Fix production cookie issues for hosted environment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -32,10 +32,10 @@ export const signUp=async (req,res)=>{
            
             })
         let token = await genToken(user._id)
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true, 
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -58,10 +58,10 @@ export const login=async(req,res)=>{
             return res.status(400).json({message:"incorrect Password"})
         }
         let token =await genToken(user._id)
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true, 
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
@@ -77,7 +77,11 @@ export const login=async(req,res)=>{
 
 export const logOut = async(req,res)=>{
     try {
-        await res.clearCookie("token")
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None"
+        })
         return res.status(200).json({message:"logOut Successfully"})
     } catch (error) {
         return res.status(500).json({message:`logout Error ${error}`})
@@ -95,10 +99,10 @@ export const googleSignup = async (req,res) => {
         })
         }
         let token =await genToken(user._id)
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true, 
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
